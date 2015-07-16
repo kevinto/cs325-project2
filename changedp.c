08b2671ac4c029ee1305277b7ad0a43c689914d1
kevinto <kevinto1989@gmail.com>
2015-07-16 08:55:06 -0700

    added arg check for greedy

--- changedp.c
+++ changedp.c
@@ -12,16 +12,6 @@
 // Program entry point
 int main(int argc, char *argv[])
 {
-	if ( argc != 2 )
-	{
-		// A file name was not entered as a program argument
-		printf( "usage: %s filename\n", argv[0] );
-	}
-	else
-	{
-		// Check if input file exists
-	}
-
 	printf("Hello world from changedp\n");
 	return 0;
 }
--- changegreedy.c
+++ changegreedy.c
@@ -1,8 +1,8 @@
 /**************************************************************
  * *  Filename: .c
- * *  Coded by: 
- * *  Purpose -  
- * * 
+ * *  Coded by:
+ * *  Purpose -
+ * *
  * ***************************************************************/
 
 #include <stdio.h>
@@ -10,8 +10,78 @@
 #include <limits.h>
 
 // Program entry point
-int main()
+int main(int argc, char *argv[])
 {
-   printf("Hello world changegreedy\n");
-   return 0;
+	if ( argc != 2 )
+	{
+		// A file name was not entered as a program argument
+		printf( "usage: %s filename\n", argv[0] );
+	}
+	else
+	{
+		// Check if input file exists
+		// Get the number of lines in the file
+		// Calculate how many problem sets are in the file.
+		//	- In the file, a problem should consist of two lines:
+		//		1. The array of denomination amounts
+		//		2. The actu
+	}
+
+	printf("Hello world changegreedy\n");
+	return 0;
+}
+
+/**************************************************************
+ * * Entry:
+ * *  inputArray - The input array to run the algo on.
+ * *  numberOfElements - The number of elements in the array
+ * *
+ * * Exit:
+ * *  n/a
+ * *
+ * * Purpose:
+ * *  Executes the divide and conquer algorithm and puts the results
+ * *  to the MSS_Results.txt file
+ * *
+ * ***************************************************************/
+void executeAlgorithm(int *inputArray, int numberOfElements)
+{
+	// int i;
+	// int currentInputArrayIdx = 0;
+	// int lastLow = 0;
+	// int lastHigh = 0;
+	// int finalSum = 0;
+
+	// // Run the divide and conquer algo
+	// maxSubArray(inputArray, 0, numberOfElements - 1, &lastLow, &lastHigh, &finalSum);
+
+	// // Check if the number of result elements is within the bounds
+	// int numberOfResultElements = lastHigh - lastLow + 1;
+	// if (numberOfResultElements <= 0)
+	// {
+	// 	printf("Error: invalid amount of result elements\n");
+	// 	return;
+	// }
+
+	// // Generate the results array
+	// int *resultArray = malloc(numberOfResultElements * sizeof(int));
+	// for (i = 0; i < numberOfResultElements; i++)
+	// {
+	// 	currentInputArrayIdx = lastLow + i;
+	// 	if ((currentInputArrayIdx >= numberOfElements) || (currentInputArrayIdx < 0))
+	// 	{
+	// 		printf("Error: Out of bounds of the input array\n");
+	// 	}
+
+	// 	resultArray[i] = inputArray[lastLow + i];
+	// }
+
+	// // Output the result to results file
+	// outputResultToFile(resultArray, numberOfResultElements, inputArray, numberOfElements);
+
+	// // For debugging purposes only
+	// // displayIntArray(resultArray, numberOfResultElements);
+	// // printf("\n");
+
+	// free(resultArray);
 }
