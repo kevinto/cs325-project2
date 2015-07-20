2031189b1cbf62376854cfa5d8b526c78e40c9e0
kevinto <kevinto1989@gmail.com>
2015-07-19 23:54:04 -0700

    added the timing file for the greedy algo

--- Makefile
+++ Makefile
@@ -10,18 +10,20 @@ SRC2 = changegreedy.c
 SRC3 = changeslow.c
 SRC4 = filefunctions.c
 SRC5 = changeslowkevin.c
+SRC6 = timer.c
 
 PROG1 = changedp 
 PROG2 = changegreedy 
 PROG3 = changeslow
 PROG4 = changeslowkevin
-PROGS = ${PROG1} ${PROG2} ${PROG3} ${PROG4}
+PROG5 = timer 
+PROGS = ${PROG1} ${PROG2} ${PROG3} ${PROG4} ${PROG5}
 
 # Not hooked up yet
 HEADER1 = filefunctions.h
 HEADERS = ${HEADER1}
 
-all: program1 program2 program3 program4
+all: program1 program2 program3 program4 program5
 
 program1: ${SRC1}
 	${CXX} ${SRC1} ${SRC4} ${HEADERS} -o ${PROG1}  
@@ -35,5 +37,8 @@ program3: ${SRC3}
 program4: ${SRC5}
 	${CXX} ${SRC5} ${SRC4} ${HEADERS} -o ${PROG4}  
 
+program5: ${SRC6}
+	${CXX} ${SRC6} -o ${PROG5}  
+
 clean:
-	rm -rf ${PROGS} *Amount.txt
+	rm -rf ${PROGS} *Amount.txt *_Results.txt
---
+++ timer.c
@@ -0,0 +1,84 @@
+/**************************************************************
+ * *  Filename: timer.c
+ * *  Coded by: Kevin To
+ * *  Purpose - To time project 2 algos
+ * ***************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <time.h>
+
+void executeGreedy(int *inputArray, int numberOfElements, int changeAmount);
+
+// Program entry point
+int main()
+{
+	clock_t timer;  
+
+	// Values for the divide and conquer
+	int array_n_vals[16] = {1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30}; 
+	int initialAmt = 100000000;
+	int numberOfIncrements = 20;
+	int changeAmount = 0;
+
+	FILE *outputGreedy = fopen("Greedy_Timing_Results.txt", "w");  //create output file for greedy algo
+
+	int i;
+	for (i = 0; i < numberOfIncrements; i++)
+	{
+		changeAmount = (i * 100000000);
+		if (changeAmount == 0)
+		{
+			continue;
+		}
+
+		// Time the greedy algo
+		timer = clock();
+		executeGreedy(array_n_vals, 16, changeAmount);
+		timer = clock() - timer;
+		fprintf(outputGreedy, "amount: %d\n", changeAmount);
+		fprintf(outputGreedy, "%f\n", (float)timer / (float)CLOCKS_PER_SEC);
+	}
+
+	// Close output files
+	fclose(outputGreedy);
+}
+
+
+/**************************************************************
+ * * Entry:
+ * *  inputArray - The input array containing the coin denominations.
+ * *  numberOfElements - The number of elements in the array.
+ * *  changeAmount - The change amount.
+ * *
+ * * Exit:
+ * *  n/a
+ * *
+ * * Purpose:
+ * *  Executes the Greedy algorithm
+ * *
+ * ***************************************************************/
+void executeGreedy(int *inputArray, int numberOfElements, int changeAmount)
+{
+	int i;
+	int *resultChangeArray = malloc(numberOfElements * sizeof(int));
+	for (i = 0; i < numberOfElements; i++)
+	{
+		resultChangeArray[i] = 0;
+	}
+
+	int minNumberOfCoins = 0;
+	for (i = numberOfElements - 1; i >= 0; i--)
+	{
+		while (inputArray[i] <= changeAmount)
+		{
+			changeAmount -= inputArray[i];
+			resultChangeArray[i]++;
+			minNumberOfCoins++;
+		}
+	}
+
+	free(resultChangeArray);
+}
